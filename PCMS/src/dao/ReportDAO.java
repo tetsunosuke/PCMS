90f220e2adec97fda5b7651749e5e545b48d19c1
nakamurakihiro <nakamurakihiro@gmail.com>
2021-08-19 23:05:27 +0900

    [fix]ReportDAOクラス内の変数名,コメント,SQL文を修正#27

--- PCMS/src/dao/ReportDAO.java
+++ PCMS/src/dao/ReportDAO.java
@@ -54,19 +54,19 @@ public class ReportDAO {
 	private List<Report> rlist = new ArrayList<>();
 
 	/**
-	 *@param number 社員番号
+	 *@param employee_id 社員ID
 	 *@return List<Report> 工数記録一覧
 	 *@throws SQLException データベース接続処理でエラー
 	 *ログインした社員の工数記録を探すメソッド
 	 */
-	public List<Report> findReport(int number) throws SQLException{
+	public List<Report> findReport(int employee_id) throws SQLException{
 
 		//データベースからログインした社員の工数記録を取得するSQL文
-		String sql ="select * from db_report where number = ?";
+		String sql ="select * from reports where employee_id = ?";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
-		ps.setInt(1,number);
+		ps.setInt(1,employee_id);
 
 		//SQL文の実行
 		rs = ps.executeQuery();
@@ -76,14 +76,14 @@ public class ReportDAO {
 			//データベースから取得した値をセット
 			Report report = new Report();
 
-			//社員番号
-			report.setNumber(rs.getInt("number"));
+			//社員ID
+			report.setEmployee_Id(rs.getInt("employee_id"));
 			//姓
-			report.setLastName(rs.getString("lastName"));
+			report.setLast_Name(rs.getString("last_name"));
 			//名
-			report.setFirstName(rs.getString("firstName"));
+			report.setFirst_Name(rs.getString("first_name"));
 			//部署コード
-			report.setDepartment_Code(rs.getString("department_code"));
+			report.setDepartment_Id(rs.getString("department_id"));
 			//日付
 			report.setDay(rs.getString("day"));
 			//機械名
@@ -106,10 +106,10 @@ public class ReportDAO {
 	}
 
 	/**
-	 *@param number 社員番号
-	 *@param lastName 姓
-	 *@param firstName 名
-	 *@param department_code 部署コード
+	 *@param employee_id 社員ID
+	 *@param last_name 姓
+	 *@param first_name 名
+	 *@param department_id 部署ID
 	 *@param day 日付
 	 *@param machine_name 機械名
 	 *@param task 作業項目
@@ -121,7 +121,7 @@ public class ReportDAO {
 	 *@throws SQLException データベース接続処理でエラー
 	 *工数記録メソッド
 	 */
-	public boolean addReport(int number,String lastName,String firstName,String department_code,String day,String machine_name,String task,int work_time,int over_time,int holiday_work,String comment) throws SQLException{
+	public boolean addReport(int employee_id,String last_name,String first_name,String department_id,String day,String machine_name,String task,int work_time,int over_time,int holiday_work,String comment) throws SQLException{
 
 		//オートコミットの無効
 		con.setAutoCommit(false);
@@ -130,18 +130,18 @@ public class ReportDAO {
 		boolean recordJudge = false;
 
 		//データベースに工数を追加記録するSQL文
-		String sql = "insert into db_report(number,lastName,firstName,department_code,day,machine_name,task,work_time,over_time,holiday_work,comment) values (?,?,?,?,?,?,?,?,?,?,?)";
+		String sql = "insert into reports (employee_id,last_name,first_name,department_id,day,machine_name,task,work_time,over_time,holiday_work,comment) values (?,?,?,?,?,?,?,?,?,?,?)";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
-		//社員番号
-		ps.setInt(1,number);
+		//社員ID
+		ps.setInt(1,employee_id);
 		//姓
-		ps.setString(2,lastName);
+		ps.setString(2,last_name);
 		//名
-		ps.setString(3,firstName);
-		//部署コード
-		ps.setString(4,department_code);
+		ps.setString(3,first_name);
+		//部署ID
+		ps.setString(4,department_id);
 		//日付
 		ps.setString(5,day);
 		//機械名
@@ -180,7 +180,7 @@ public class ReportDAO {
 		con.setAutoCommit(false);
 
 		//データベースの工数記録を更新するSQL文
-		String sql = "update db_report set machine_name = ?,work_time = ?,over_time = ?,holiday_work = ?,task = ? where number = ? and day = ?";
+		String sql = "update reports set machine_name = ?,work_time = ?,over_time = ?,holiday_work = ?,task = ? where employee_id = ? and day = ?";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
@@ -194,8 +194,8 @@ public class ReportDAO {
 		ps.setInt(4,report.getHoliday_Work());
 		//作業内容
 		ps.setString(5,report.getTask());
-		//社員番号
-		ps.setInt(6,report.getNumber());
+		//社員ID
+		ps.setInt(6,report.getEmployee_Id());
 		//日付
 		ps.setString(7,report.getDay());
 
@@ -210,27 +210,27 @@ public class ReportDAO {
 	}
 
 	/**
-	 *@param number 社員番号
+	 *@param employee_id 社員ID
 	 *@param day 日付
 	 *@return 選択した工数記録を削除出来たらtrue,削除出来なかったらfalse
 	 *@throws SQLException データベース接続処理でエラー
 	 *選択した工数記録を削除するメソッド
 	 */
-	public boolean deleteReport(int number,String day) throws SQLException{
+	public boolean deleteReport(int employee_id,String day) throws SQLException{
 
 		//オートコミットの無効
 		con.setAutoCommit(false);
 
 		//データベースから選択した工数記録を削除するSQL文
-		String sql = "delete from db_report where number = ? and day = ?";
+		String sql = "delete from reports where employee_id = ? and day = ?";
 		ps = con.prepareStatement(sql);
 
 		//削除判定
 		boolean deleteJudge = false;
 
 		//プレースホルダに値をセット
-		//社員番号
-		ps.setInt(1,number);
+		//社員ID
+		ps.setInt(1,employee_id);
 		//日付
 		ps.setString(2,day);
 
@@ -246,24 +246,24 @@ public class ReportDAO {
 	}
 
 	/**
-	 *@param number 社員番号
+	 *@param employee_id 社員ID
 	 *@param day 日付
 	 *@return 選択した工数記録を返す
 	 *@throws SQLException データベース接続処理でエラー
 	 *選択した工数情報を探すメソッド
 	 */
-	public Report selectReport(int number,String day) throws SQLException{
+	public Report selectReport(int employee_id,String day) throws SQLException{
 
 		//初期化
 		Report report = null;
 
 		//データベースから工数記録を取得するSQL文
-		String sql ="select * from db_report where number = ? and day = ?";
+		String sql ="select * from reports where employee_id = ? and day = ?";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
-		//社員番号
-		ps.setInt(1,number);
+		//社員ID
+		ps.setInt(1,employee_id);
 		//日付
 		ps.setString(2,day);
 
@@ -275,14 +275,14 @@ public class ReportDAO {
 			//データベースから取得した値をセット
 			report = new Report();
 
-			//社員番号の取得
-			report.setNumber(rs.getInt("number"));
+			//社員IDの取得
+			report.setEmployee_Id(rs.getInt("employee_id"));
 			//姓の取得
-			report.setLastName(rs.getString("lastName"));
+			report.setLast_Name(rs.getString("last_name"));
 			//名の取得
-			report.setFirstName(rs.getString("firstName"));
-			//部署コードの取得
-			report.setDepartment_Code(rs.getString("department_code"));
+			report.setFirst_Name(rs.getString("first_name"));
+			//部署IDの取得
+			report.setDepartment_Id(rs.getString("department_id"));
 			//日付の取得
 			report.setDay(rs.getString("day"));
 			//機械名の取得
@@ -303,20 +303,20 @@ public class ReportDAO {
 	}
 
 	/**
-	 *@param department_code 部署コード
+	 *@param department_id 部署ID
 	 *@param machine_name 機械名
 	 *@return List<Report> 工数記録一覧
 	 *@throws SQLException データベース接続処理でエラー
 	 *所属部署と機械名毎に工数記録を取得するメソッド
 	 */
-	public List<Report> eachReport(String department_code,String machine_name) throws SQLException{
+	public List<Report> eachReport(String department_id,String machine_name) throws SQLException{
 
 		//データベースから工数記録を取得するSQL文
-		String sql ="select lastName,firstName,day,task,work_time,over_time,holiday_work,comment from db_report where department_code = ? and machine_name = ? order by day";
+		String sql ="select last_name,first_name,day,task,work_time,over_time,holiday_work,comment from reports where department_id = ? and machine_name = ? order by day";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
-		ps.setString(1,department_code);
+		ps.setString(1,department_id);
 		ps.setString(2,machine_name);
 
 		//SQL文の実行
@@ -327,9 +327,9 @@ public class ReportDAO {
 			//データベースから取得した値をセット
 			Report report = new Report();
 			//姓
-			report.setLastName(rs.getString(1));
+			report.setLast_Name(rs.getString(1));
 			//名
-			report.setFirstName(rs.getString(2));
+			report.setFirst_Name(rs.getString(2));
 			//日付
 			report.setDay(rs.getString(3));
 			//作業内容
@@ -349,22 +349,22 @@ public class ReportDAO {
 	}
 
 	/**
-	 *@param number 社員番号
+	 *@param employee_id 社員ID
 	 *@return  List<Report> 工数記録一覧
 	 *@throws SQLException データベース接続処理でエラー
 	 *選択した社員の工数記録を全件表示するメソッド
 	 */
-	public List<Report> selectAllReport(int number) throws SQLException{
+	public List<Report> selectAllReport(int employee_id) throws SQLException{
 
 		//初期化
 		Report report = null;
 
 		//データベースから選択した社員の工数記録を取得するSQL文
-		String sql ="select lastName,firstName,day,machine_name,task,work_time,over_time,holiday_work,comment from db_report where number = ? order by day";
+		String sql ="select last_name,first_name,day,machine_name,task,work_time,over_time,holiday_work,comment from reports where employee_id = ? order by day";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
-		ps.setInt(1,number);
+		ps.setInt(1,employee_id);
 
 		//SQL文の実行
 		rs = ps.executeQuery();
@@ -375,9 +375,9 @@ public class ReportDAO {
 			report = new Report();
 
 			//姓
-			report.setLastName(rs.getString(1));
+			report.setLast_Name(rs.getString(1));
 			//名
-			report.setFirstName(rs.getString(2));
+			report.setFirst_Name(rs.getString(2));
 			//日付
 			report.setDay(rs.getString(3));
 			//機械名
@@ -410,7 +410,7 @@ public class ReportDAO {
 		Report report = null;
 
 		//データベースから選択した機械名の工数記録を取得するSQL文
-		String sql ="select machine_name,work_time,over_time,holiday_work,comment from db_report where machine_name = ?";
+		String sql ="select machine_name,work_time,over_time,holiday_work,comment from reports where machine_name = ?";
 		ps = con.prepareStatement(sql);
 
 		//プレースホルダに値をセット
@@ -452,7 +452,7 @@ public class ReportDAO {
 		Report report = null;
 
 		//データベースから全社員の工数記録を取得するSQL文
-		String sql ="select number,lastName,firstName,day,machine_name,task,work_time,over_time,holiday_work,comment from db_report order by day";
+		String sql ="select employee_id,last_name,first_name,day,machine_name,task,work_time,over_time,holiday_work,comment from reports order by day";
 		ps = con.prepareStatement(sql);
 
 		//SQL文の実行
@@ -463,12 +463,12 @@ public class ReportDAO {
 			//データベースから取得した値をセット
 			report = new Report();
 
-			//社員番号
-			report.setNumber(rs.getInt("number"));
+			//社員ID
+			report.setEmployee_Id(rs.getInt("employee_id"));
 			//姓
-			report.setLastName(rs.getString("lastName"));
+			report.setLast_Name(rs.getString("last_name"));
 			//名
-			report.setFirstName(rs.getString("firstName"));
+			report.setFirst_Name(rs.getString("first_name"));
 			//日付
 			report.setDay(rs.getString("day"));
 			//機械名
