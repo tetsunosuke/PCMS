534d141068a638666a0da5bb7d86ad32e856f1cc
nakamurakihiro <nakamurakihiro@gmail.com>
2021-08-20 23:26:09 +0900

    [fix]WorkAddクラス内の変数名,コメントを修正#27

--- PCMS/src/servlet/WorkAdd.java
+++ PCMS/src/servlet/WorkAdd.java
@@ -39,7 +39,7 @@ public class WorkAdd extends HttpServlet{
 	*@param response Servletがクライアントに返すレスポンス内容を含むHttpServletResponseオブジェクト
 	*@throws ServletException ServletがPostリクエストを処理中にServlet内で例外が発生
 	*@throws IOException ServletがPostリクエストを処理中に入出力エラーが発生
-	*ログインしている社員の部署コードを取得する。<br>
+	*ログインしている社員の部署IDを取得する。<br>
 	*データベースに接続して該当部署に記入した作業項目を追加登録する。<br>
 	*作業項目追加登録完了画面へ画面偏移する。
 	*/
@@ -48,9 +48,9 @@ public class WorkAdd extends HttpServlet{
 		//セッションオブジェクトの開始
 		HttpSession session = request.getSession();
 
-		//ログインした社員の部署コードを取得
+		//ログインした社員の部署IDを取得
 		Employee syain = (Employee)session.getAttribute("employee");
-		String department_code = syain.getDepartment_Code();
+		String department_id = syain.getDepartment_Id();
 
 		//追加作業項目を取得
 		String task = request.getParameter("task");
@@ -66,7 +66,7 @@ public class WorkAdd extends HttpServlet{
 			wd.dbConnect();
 
 			//作業項目の追加登録
-			addJudge = wd.addTask(department_code,task);
+			addJudge = wd.addTask(department_id,task);
 
 		}catch (SQLException e){
 			e.printStackTrace();
